9310d6716953c6d54a59c20e189e30747fa79df6
noobedy <maxwellzhang2011@gmail.com>
2026-05-20 20:28:12 -0500

    more updates
    use alt wasd to move and jj to exit insert space is leader <leader>xx to delete line and < > to resize window and alt + shifit+wasd to swithc window <leader>s(h for horzontal v for vertical) <leader>t(h for horzongal v for vertical)

--- noob/keybind.lua
+++ noob/keybind.lua
@@ -1,13 +1,32 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
-vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>fe", ":Lexplore<CR>")
+vim.keymap.set("n", "<leader>ws", ":w<CR>")
+vim.keymap.set({"i", "n"}, "jj", "<Esc>")
 vim.keymap.set("n", "<leader>wq", ":wq<CR>")
 vim.keymap.set("n", "<leader>jq", ":q<CR>")
 vim.keymap.set("n", "<leader>qf", ":q!<CR>")
 vim.keymap.set("n", "<leader>xx", "dd")
+vim.keymap.set("n", "<<", "<C-w><")
+vim.keymap.set("n", ">>", "<C-w>>")
+vim.keymap.set("n", "=", "<C-w>+")
+vim.keymap.set("n", "-", "<C-w>-")
+vim.keymap.set("n", "<A-W>", "<C-w><Up>")
+vim.keymap.set("n", "<A-A>", "<C-w><Left>")
+vim.keymap.set("n", "<A-S>", "<C-w><Down>")
+vim.keymap.set("n", "<A-D>", "<C-w><Right>")
+vim.keymap.set("n", "<leader>sh", ":split<CR>")
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>tv", ":split | term<CR>")
+vim.keymap.set("n", "<leader>th", ":vsplit | term<CR>")
+vim.keymap.set("t", "<C-e>", "<C-\\><C-n>")
+vim.keymap.set('n', '<leader>cd', ':cd %:p:h<CR>:pwd<CR>', { noremap = true })
 
 vim.keymap.set({"i", "n", "v"}, "<A-w>", "<Up>")
 vim.keymap.set({"i", "n", "v"}, "<A-a>", "<Left>")
 vim.keymap.set({"i", "n", "v"}, "<A-s>", "<Down>")
 vim.keymap.set({"i", "n", "v"}, "<A-d>", "<Right>")
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.shiftround = true
--- noob/theme.lua
+++ noob/theme.lua
@@ -1,12 +1,42 @@
 vim.opt.termguicolors = true
 
 -- bg color
-vim.api.nvim_set_hl(0, "Normal", { bg = "#212121" })
+vim.api.nvim_set_hl(0, "Normal", { fg = "#FFFFFF", bg = "#212121" })
 
 -- sytex highlight
-vim.api.nvim_set_hl(0, "Comment", {fg = "#6B9E67" })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#6B9E67" })
 vim.api.nvim_set_hl(0, "String", { fg = "#77D17C" })
-vim.api.nvim_set_hl(0, "Functions", { fg="#F0F0F0"})
-vim.api.nvim_set_hl(0, "Keyword", { fg="#3DFFFF" } )
-vim.api.nvim_set_hl(0, "Character", { fg="#77D17C"})
-vim.api.nvim_set_hl(0, "SpecialChar",{fg="#BFBFBF"})
+vim.api.nvim_set_hl(0, "Function", { fg = "#F0F0F0" })
+vim.api.nvim_set_hl(0, "Keyword", { fg = "#3DFFFF" })
+vim.api.nvim_set_hl(0, "Character", { fg = "#77D17C" })
+vim.api.nvim_set_hl(0, "SpecialChar", { fg = "#BFBFBF" })
+vim.api.nvim_set_hl(0, "Number", { fg = "#BD93F9" })
+vim.api.nvim_set_hl(0, "Boolean", { fg = "#BD93F9" })
+vim.api.nvim_set_hl(0, "Constant", { fg = "#77D17C" })
+vim.api.nvim_set_hl(0, "Identifier", { fg = "#8BE9FD" })
+vim.api.nvim_set_hl(0, "Statement", { fg = "#FF79C6" })
+vim.api.nvim_set_hl(0, "Conditional", { fg = "#FF79C6" })
+vim.api.nvim_set_hl(0, "Repeat", { fg = "#FF79C6" })
+vim.api.nvim_set_hl(0, "Operator", { fg = "#FF79C6" })
+vim.api.nvim_set_hl(0, "Type", { fg = "#8BE9FD" })
+vim.api.nvim_set_hl(0, "PreProc", { fg = "#FFB86C" })
+vim.api.nvim_set_hl(0, "Special", { fg = "#F1FA8C" })
+vim.api.nvim_set_hl(0, "Delimiter", { fg = "#F8F8F2" })
+vim.api.nvim_set_hl(0, "Error", { fg = "#FF5555" })
+vim.api.nvim_set_hl(0, "Todo", { fg = "#FFB86C" })
+
+-- outter theme
+vim.opt.cursorline = true
+
+-- file manager
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.api.nvim_set_hl(0, "netrwDir", { fg = "#8BE9FD" })
+vim.api.nvim_set_hl(0, "netrwClassify", { fg = "#8BE9FD" })
+vim.api.nvim_set_hl(0, "netrwLink", { fg = "#F1FA8C" })
+vim.api.nvim_set_hl(0, "netrwSymLink", { fg = "#F1FA8C" })
+vim.api.nvim_set_hl(0, "netrwExe", { fg = "#50FA7B" })
+vim.api.nvim_set_hl(0, "netrwList", { fg = "#F8F8F2" })
+vim.api.nvim_set_hl(0, "netrwTree", { fg = "#6272A4" })
+vim.api.nvim_set_hl(0, "netrwMarkFile", { bg = "#44475A" })
+vim.api.nvim_set_hl(0, "netrwHide", { fg = "#6272A4" })
