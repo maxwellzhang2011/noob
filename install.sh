bdae5b83b0bac287b255a52d4c530a6fa845fa35
noobedy <maxwellzhang2011@gmail.com>
2026-05-22 23:04:15 -0500

    install.sh updated
    added neo-tree installing support

--- install.sh
+++ install.sh
@@ -2,10 +2,24 @@
 
 case "$1" in
     version)
-        echo "0.0.1"
+        echo "0.1.2"
         ;;
+    ntre_inst)
+	NVIM_DATA="${XDG_DATA_HOME:-$HOME/.local/share}/nvim"
+	PLUGIN_DIR="$NVIM_DATA/site/pack/plugins/start"
 
-    *)
+	echo "Creating plugin directory..."
+	mkdir -p "$PLUGIN_DIR"
+
+	echo "Cloning Neo-tree and dependencies..."
+	git clone https://github.com/nvim-lua/plenary.nvim "$PLUGIN_DIR/plenary.nvim"
+	git clone https://github.com/nvim-tree/nvim-web-devicons "$PLUGIN_DIR/nvim-web-devicons"
+	git clone https://github.com/MunifTanjim/nui.nvim "$PLUGIN_DIR/nui.nvim"
+	git clone https://github.com/nvim-neo-tree/neo-tree.nvim "$PLUGIN_DIR/neo-tree.nvim"
+
+	echo "neo-tree installed"
+	;;
+    noob_inst)
         echo "installing noob neovim package"
 
         if [ ! -d "$HOME/.config/nvim" ]; then
@@ -36,7 +50,54 @@ case "$1" in
             echo "installing require"
             echo 'require("noob")' >> "$HOME/.config/nvim/init.lua"
         fi
+        echo "noob installed, finish launch nvim"
+        echo "report any bugs or wanted add ons in github"
+        ;;
+    full)
+        NVIM_DATA="${XDG_DATA_HOME:-$HOME/.local/share}/nvim"
+	PLUGIN_DIR="$NVIM_DATA/site/pack/plugins/start"
 
+	echo "Creating plugin directory..."
+	mkdir -p "$PLUGIN_DIR"
+
+	echo "Cloning Neo-tree and dependencies..."
+	git clone https://github.com/nvim-lua/plenary.nvim "$PLUGIN_DIR/plenary.nvim"
+	git clone https://github.com/nvim-tree/nvim-web-devicons "$PLUGIN_DIR/nvim-web-devicons"
+	git clone https://github.com/MunifTanjim/nui.nvim "$PLUGIN_DIR/nui.nvim"
+	git clone https://github.com/nvim-neo-tree/neo-tree.nvim "$PLUGIN_DIR/neo-tree.nvim"
+
+	echo "neo-tree installed"
+	;;
+	echo "installing noob neovim package"
+
+        if [ ! -d "$HOME/.config/nvim" ]; then
+            echo "building nvim folder"
+            mkdir -p "$HOME/.config/nvim"
+        fi
+
+        if [ ! -f "$HOME/.config/nvim/init.lua" ]; then
+            echo "setting up init.lua"
+            touch "$HOME/.config/nvim/init.lua"
+        fi
+
+        if [ ! -d "$HOME/.config/nvim/lua" ]; then
+            echo "building package folder"
+            mkdir -p "$HOME/.config/nvim/lua"
+	fi
+
+        if [ -d "$HOME/.config/nvim/lua/noob" ]; then
+            echo "removing older version of noob"
+            rm -rf "$HOME/.config/nvim/lua/noob"
+        fi
+
+        mv noob "$HOME/.config/nvim/lua/"
+
+        if grep -q 'require("noob")' "$HOME/.config/nvim/init.lua"; then
+            echo "require installed"
+        else
+            echo "installing require"
+            echo 'require("noob")' >> "$HOME/.config/nvim/init.lua"
+        fi
         echo "noob installed, finish launch nvim"
         echo "report any bugs or wanted add ons in github"
         ;;
